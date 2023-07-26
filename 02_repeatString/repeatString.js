684b4ab01f2ceb5c8e71bfe2a0dc973381208a2a
Ela Bogucka <9089666+ebogucka@users.noreply.github.com>
2023-07-26 19:15:58 +0200

    add 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, repetitions) {
+  if (repetitions < 0) return 'ERROR';
 
+  result = '';
+  for (let i = 0; i < repetitions; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
