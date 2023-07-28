f0298e752332c0de57ec48f4230198a030dd46c4
Ela Bogucka <9089666+ebogucka@users.noreply.github.com>
2023-07-28 22:16:17 +0200

    add 04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...itemsToRemove) {
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    const index = array.indexOf(itemsToRemove[i]);
+    if (index > -1) array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
